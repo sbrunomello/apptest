45d623382ecee1f6d1c757f062ab9ee522d5540d
Bruno Mello <sbrunomello@gmail.com>
2019-07-29 22:01:07 -0300

    Adicionando Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/brunomello/apptest/ApptestApplication.java
+++ src/main/java/com/brunomello/apptest/ApptestApplication.java
@@ -1,5 +1,6 @@
 package com.brunomello.apptest;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.brunomello.apptest.domain.Cidade;
 import com.brunomello.apptest.domain.Cliente;
 import com.brunomello.apptest.domain.Endereco;
 import com.brunomello.apptest.domain.Estado;
+import com.brunomello.apptest.domain.Pagamento;
+import com.brunomello.apptest.domain.PagamentoComBoleto;
+import com.brunomello.apptest.domain.PagamentoComCartao;
+import com.brunomello.apptest.domain.Pedido;
 import com.brunomello.apptest.domain.Produto;
+import com.brunomello.apptest.domain.enums.EstadoPagamento;
 import com.brunomello.apptest.domain.enums.TipoCliente;
 import com.brunomello.apptest.repositories.CategoriaRepository;
 import com.brunomello.apptest.repositories.CidadeRepository;
 import com.brunomello.apptest.repositories.ClienteRepository;
 import com.brunomello.apptest.repositories.EnderecoRepository;
 import com.brunomello.apptest.repositories.EstadoRepository;
+import com.brunomello.apptest.repositories.PagamentoRepository;
+import com.brunomello.apptest.repositories.PedidoRepository;
 import com.brunomello.apptest.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -43,6 +51,11 @@ public class ApptestApplication implements CommandLineRunner {
 	@Autowired
 	private EnderecoRepository enderecoRepository;
 	
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 	
 	
 	public static void main(String[] args) {
@@ -104,6 +117,21 @@ public class ApptestApplication implements CommandLineRunner {
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2018 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("15/10/2018 09:02"), cli1, e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("25/10/2018 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+		
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 		
 		
 	}
